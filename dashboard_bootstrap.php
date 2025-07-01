08278e2965401e6a08255d74bb70cdc826c831b6
Gideon Okta Dinata <gydeeesi@gmail.com>
2025-07-02 03:33:07 +0700

    UAS TERBARU

--- dashboard_bootstrap.php
+++ dashboard_bootstrap.php
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_credentials'])
   <body>
     
 <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">My Dashboard</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Dashboard</a>
   <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse"
           data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
           aria-label="Toggle navigation">
@@ -77,89 +77,93 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_credentials'])
 <div class="container-fluid">
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3 sidebar-sticky">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              <span data-feather="home" class="align-text-bottom"></span>
-              Dashboard
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file" class="align-text-bottom"></span>
-              Orders
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="shopping-cart" class="align-text-bottom"></span>
-              Products
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users" class="align-text-bottom"></span>
-              Customers
-            </a>
-            <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users" class="align-text-bottom"></span>
-              Reports
-            </a>
-            <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users" class="align-text-bottom"></span>
-              Integrations
-            </a>
-          </li>
-        </ul>
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-  <span>Saved reports</span>
-  <a class="link-secondary" href="#" aria-label="Add a new report">
-    <span data-feather="plus-circle" class="align-text-bottom"></span>
-  </a>
-</h6>
-<ul class="nav flex-column mb-2">
-  <li class="nav-item">
-    <a class="nav-link" href="#">
-      <span data-feather="file-text" class="align-text-bottom"></span>
-      Current month
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">
-      <span data-feather="file-text" class="align-text-bottom"></span>
-      Last quarter
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">
-      <span data-feather="file-text" class="align-text-bottom"></span>
-      Social engagement
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">
-      <span data-feather="file-text" class="align-text-bottom"></span>
-      Year-end sale
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#settings">
-  <span data-feather="settings" class="align-text-bottom"></span>
-  Pengaturan
-</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link text-danger" href="logout_bootstrap.php">
-  <span data-feather="log-out" class="align-text-bottom"></span>
-  Sign out
-</a>
-  </li>
-</ul>
-      </div>
-    </nav>
+  <div class="position-sticky pt-3 sidebar-sticky">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">
+          <span data-feather="home" class="align-text-bottom"></span>
+          Dashboard
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="file-text" class="align-text-bottom"></span>
+          Orders
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="shopping-cart" class="align-text-bottom"></span>
+          Products
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="users" class="align-text-bottom"></span>
+          Customers
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="bar-chart-2" class="align-text-bottom"></span>
+          Reports
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="layers" class="align-text-bottom"></span>
+          Integrations
+        </a>
+      </li>
+    </ul>
+
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+      <span>Saved reports</span>
+      <a class="link-secondary" href="#" aria-label="Add a new report">
+        <span data-feather="plus-circle" class="align-text-bottom"></span>
+      </a>
+    </h6>
+    <ul class="nav flex-column mb-2">
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="calendar" class="align-text-bottom"></span>
+          Current month
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="clock" class="align-text-bottom"></span>
+          Last quarter
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="message-square" class="align-text-bottom"></span>
+          Social engagement
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="bar-chart" class="align-text-bottom"></span>
+          Year-end sale
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#settings">
+          <span data-feather="settings" class="align-text-bottom"></span>
+          Pengaturan
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-danger" href="logout_bootstrap.php">
+          <span data-feather="log-out" class="align-text-bottom"></span>
+          Sign out
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -167,7 +171,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_credentials'])
       </div>
 
       <!-- Grafik area -->
-      <h4>Sales Chart</h4>
+      <h4>Grafik</h4>
       <div class="chart-container my-4">
         <canvas id="myChart"></canvas>
       </div>
@@ -225,6 +229,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_credentials'])
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.min.js"></script>
 <script src="dashboard.js"></script>
+
+<script src="https://unpkg.com/feather-icons"></script>
+<script>
+  feather.replace()
+</script>
 
   </body>
 </html>
